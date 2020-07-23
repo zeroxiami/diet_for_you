3755855529cad94fe004de037ebb31eb32401b4f
Alexander Magana <alexander.jm28@gmail.com>
2020-07-22 22:11:34 -0500

    fixed file strucutre

--- diet_for_you/urls.py
+++ diet_for_you/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(post_info, views.post_info)
 ]
--- views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def post_info():
-    #button action here
-    return null
