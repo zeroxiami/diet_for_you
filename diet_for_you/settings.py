9757aded336c665c3fc07b6e01ad3a3ef91a999f
Alexander Magana <alexander.jm28@gmail.com>
2020-08-05 16:20:18 -0500

    now give new users a reccommended diet and fixed home page

--- diet_for_you/settings.py
+++ diet_for_you/settings.py
@@ -119,4 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/images',
+]
 LOGIN_REDIRECT_URL = "http://127.0.0.1:8000/main/"
